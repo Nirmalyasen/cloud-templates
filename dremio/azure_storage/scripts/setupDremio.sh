085eb4dbb9ac2ec55ad8bf387bec8466396a41ee
Nirmalya Sen <nsen@dremio.com>
2019-06-04 12:44:54 -0700

    configure dremio

--- dremio/azure_storage/scripts/setupDremio.sh
+++ dremio/azure_storage/scripts/setupDremio.sh
@@ -13,12 +13,16 @@ if [ -z "$service" ]; then
    echo "Require the service to start - master, coordinator or executor"
    exit 1
 fi
+storage_account=$2
+access_key=$3
 
 # In Azure, /dev/sdb is ephemeral storage mapped to /mnt/resource.
 # Additional disks are mounted after that...
 DISK_NAME=/dev/sdc
 DISK_PART=${DISK_NAME}1
-DREMIO_CONFIG_FILE=/etc/dremio/dremio.conf
+DREMIO_HOME=/opt/dremio
+DREMIO_CONFIG_DIR=/etc/dremio
+DREMIO_CONFIG_FILE=$DREMIO_CONFIG_DIR/dremio.conf
 DREMIO_DATA_DIR=/var/lib/dremio
 # Azure Linux VMs have ephemeral/temporary disk
 # always mounted on /mnt/resource/dremio
@@ -74,6 +78,14 @@ function upgrade_master {
 }
 
 function setup_master {
+  if [ -n "$storage_account" -a -n "$access_key" ]; then
+    storage_create_action "dremiodata" filesystem && \
+    storage_create_action "dremiodata/accelerator" directory && \
+    storage_create_action "dremiodata/uploads" directory && \
+    write_coresite_xml && \
+    configure_dremio_storage
+  fi
+
   sed -i "s/executor.enabled: true/executor.enabled: false/" $DREMIO_CONFIG_FILE
   upgrade_master
 }
@@ -96,6 +108,82 @@ function setup_executor {
   echo "zookeeper: \"$zookeeper:2181\"" >> $DREMIO_CONFIG_FILE
 }
 
+function storage_create_action {
+  resource=$1
+  resource_type=$2
+  blob_store_url="dfs.core.windows.net"
+  authorization="SharedKey"
+  request_method="PUT"
+  request_date=$(TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")
+  storage_service_version="2018-11-09"
+  # HTTP Request headers
+  x_ms_date_h="x-ms-date:$request_date"
+  x_ms_version_h="x-ms-version:$storage_service_version"
+  content_length_h="Content-Length: 0"
+  # Build the signature string
+  canonicalized_headers="${x_ms_date_h}\n${x_ms_version_h}"
+  canonicalized_resource="/${storage_account}/${resource}\nresource:${resource_type}"
+  string_to_sign="${request_method}\n\n\n\n\n\n\n\n\n\n\n\n${canonicalized_headers}\n${canonicalized_resource}"
+  # Decode the Base64 encoded access key, convert to Hex.
+  decoded_hex_key="$(echo -n $access_key | base64 -d -w0 | xxd -p -c256)"
+  # Create the HMAC signature for the Authorization header
+  signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$decoded_hex_key" -binary |  base64 -w0)
+  authorization_header="Authorization: $authorization $storage_account:$signature"
+  curl \
+    -X $request_method \
+    -H "$content_length_h" \
+    -H "$x_ms_date_h" \
+    -H "$x_ms_version_h" \
+    -H "$authorization_header" \
+    "https://${storage_account}.${blob_store_url}/${resource}?resource=${resource_type}"
+  return $?
+}
+
+function write_coresite_xml {
+
+cat > $DREMIO_CONFIG_DIR/core-site.xml <<EOF
+<?xml version="1.0"?>
+<configuration>
+<property>
+ <name>fs.dremioAzureStorage.impl</name>
+ <description>FileSystem implementation. Must always be com.dremio.plugins.azure.AzureStorageFileSystem</description>
+ <value>com.dremio.plugins.azure.AzureStorageFileSystem</value>
+</property>
+<property>
+  <name>dremio.azure.account</name>
+  <description>The name of the storage account.</description>
+  <value>$storage_account</value>
+</property>
+<property>
+  <name>dremio.azure.key</name>
+  <description>The shared access key for the storage account.</description>
+  <value>$access_key</value>
+</property>
+<property>
+  <name>dremio.azure.mode</name>
+  <description>The storage account type. Value: STORAGE_V2</description>
+  <value>STORAGE_V2</value>
+</property>
+<property>
+  <name>dremio.azure.secure</name>
+  <description>Boolean option to enable SSL connections. Value: True/False</description>
+  <value>ENABLE_SSL</value>
+</property>
+</configuration>
+EOF
+
+}
+
+function configure_dremio_storage {
+cat >> $DREMIO_CONFIG_FILE <<EOF
+
+paths.accelerator: "dremioAzureStorage://:///dremiodata/accelerator"
+paths.uploads: "dremioAzureStorage://:///dremiodata/uploads"
+EOF
+}
+
 setup_$service
-service dremio start
-chkconfig dremio on
+cp $DREMIO_HOME/share/dremio/dremio.service /etc/systemd/system
+systemctl daemon-reload
+systemctl start dremio
+systemctl enable dremio
